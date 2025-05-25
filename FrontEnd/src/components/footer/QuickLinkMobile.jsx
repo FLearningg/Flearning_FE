f88fe18ae8e1dbc805fb7a412b3122edc12f6e5a
Trung <trungnnde180575@fpt.edu.vn>
2025-05-25 18:21:41 +0700

    [UI][fix] Fix missing dropdown in navbar
    fix missing dropdown of notification and wishlist in navbar
    fix change navbar to fixed position
    fix footer margin top

--- FrontEnd/src/components/footer/QuickLinkMobile.jsx
+++ FrontEnd/src/components/footer/QuickLinkMobile.jsx
@@ -3,17 +3,16 @@ import { Link } from 'react-router-dom'
 function QuickLinkMobile() {
     return (
         <>
-            <div class="accordion mobile-view mb-3" id="accordionPanelsStayOpenExample" data-bs-theme="dark">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            <div className="accordion mobile-view mb-3" id="accordionPanelsStayOpenExample" data-bs-theme="dark">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
                             TOP 4 CATEGORY
                         </button>
                     </h2>
-                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                        <div class="accordion-body">
+                    <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                        <div className="accordion-body">
                             <ul className="nav flex-column">
-                                {/* <li className="nav-item nav-item1 mb-2">Development<a href="#" className="nav-link p-0 fw-bold">1800.6616 (8h00 - 22h00)</a></li> */}
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">Development</Link></li>
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">Finance & Accounting</Link></li>
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">Design</Link></li>
@@ -22,14 +21,14 @@ function QuickLinkMobile() {
                         </div>
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
                             QUICK LINKS
                         </button>
                     </h2>
-                    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                        <div class="accordion-body">
+                    <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                        <div className="accordion-body">
                             <ul className="nav flex-column">
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">About</Link></li>
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">Become Instructor</Link></li>
@@ -39,14 +38,14 @@ function QuickLinkMobile() {
                         </div>
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="panelsStayOpen-headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
                             SUPPORT
                         </button>
                     </h2>
-                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                        <div class="accordion-body">
+                    <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                        <div className="accordion-body">
                             <ul className="nav flex-column">
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">Help Center</Link></li>
                                 <li className="nav-item mb-2"><Link to="#" className="nav-link p-0">FAQs</Link></li>
