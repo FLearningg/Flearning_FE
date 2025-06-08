e3e4f2585550db2808e178c5ed9b8956643e4781
Nguyễn Ngọc Trung <trungnnde180575@fpt.edu.vn>
2025-06-08 14:53:06 +0700

    [FLN-20][UI][14_Student_Wishlist] Add StudentWishlist (#28)

--- FrontEnd/src/routes/mainRoutes.jsx
+++ FrontEnd/src/routes/mainRoutes.jsx
@@ -10,7 +10,9 @@ import StudentDashboard from '../components/StudentDashboard/StudentDashboard';
 import PurchaseHistory from '../components/PurchaseHistory/PurchaseHistory';
 import { StudentMessage } from '../components/StudentMessage/StudentMessage';
 import ProfileSection from '../components/CourseList/ProfileSection';
+import WishListPage from '../components/StudentWishList/WishListPage';
 import ProfileSetting from '../components/ProfileSetting/ProfileSetting';
+
 
 // Temporary placeholder components
 const Teachers = () => {
@@ -32,25 +34,6 @@ const Teachers = () => {
   );
 };
 
-const Wishlist = () => {
-  const location = useLocation();
-  return (
-    <div className="profile-container">
-      <ProfileSection
-        avatar="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=150&h=150&q=80"
-        name="Kevin Gilbert"
-        title="Web Designer & Best-Selling Instructor"
-        activePath={location.pathname}
-        showMobileHeader={false}
-      />
-      <div className="page-content">
-        <h1>Wishlist</h1>
-        <p>Wishlist page coming soon...</p>
-      </div>
-    </div>
-  );
-};
-
 const mainRoutesContent = (
   <>
     <Route index element={<HomePage />} />
@@ -59,7 +42,7 @@ const mainRoutesContent = (
     <Route path="profile/courses" element={<CourseList />} />
     <Route path="profile/teachers" element={<Teachers />} />
     <Route path="profile/message" element={<StudentMessage />} />
-    <Route path="profile/wishlist" element={<Wishlist />} />
+    <Route path="profile/wishlist" element={<WishListPage />} />
     <Route path="profile/purchase-history" element={<PurchaseHistory />} />
     <Route path="profile/settings" element={<ProfileSetting />} />
     {/* <Route path="courses" element={<CoursesPage />} /> */}
