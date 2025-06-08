36135cbc623a56ed5e0ec2dd660f1dd4e7405374
Ryuseikai <kthkun@gmail.com>
2025-06-08 09:06:08 +0700

    [FLN-39][Dat][UI] Add Charts in AdminEarning UI [08.06.2025] (#26)

--- FrontEnd/src/routes/adminRoutes.jsx
+++ FrontEnd/src/routes/adminRoutes.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import AdminDashboard from "../components/AdminDashboard/AdminDashboard";
+import AdminEarning from "../components/AdminEarning/AdminEarning";
 
 const adminRoutesContent = // Đây là JSX, không phải là một component
   <Route path="dashboard" element={<AdminDashboard title="Dashboard" />} />;
-
+  <Route path="earning" element={<AdminEarning title="Earning" />} />;
 export default adminRoutesContent; // Export trực tiếp JSX fragment
