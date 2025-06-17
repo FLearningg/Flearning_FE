c582b75522ae752ca1505d19b9ff986b9b501dce
Nomsociuu <nomsociuu2004@gmail.com>
2025-06-18 01:40:38 +0700

    [FLN-73][Thien][UI] Fix Card UI in Catergories components #18.06.2025

---
+++ FrontEnd/src/components/Categories/CategoryPage.jsx
@@ -0,0 +1,33 @@
+import BestSelling from "./BestSelling";
+import PopularKeywords from "./PopularKeywords";
+import PopularTools from "./PopularTools";
+import CourseScreen from "./CourseScreen";
+
+const CategoryPage = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginLeft: "200px",
+                marginRight: "200px",
+            }}
+        >
+            <div style={{ width: "100%" }}>
+                <BestSelling />
+            </div>
+            <div style={{ width: "100%" }}>
+                <PopularKeywords />
+            </div>
+            <div style={{ width: "100%" }}>
+                <PopularTools />
+            </div>
+            <div style={{ width: "100%" }}>
+                <CourseScreen />
+            </div>
+        </div>
+    );
+};
+
+export default CategoryPage;
