ee14468d157cfa127816b0981c214af7c860b472
Nomsociuu <nomsociuu2004@gmail.com>
2025-06-27 14:36:31 +0700

    [FLN-77][Thien][UI] Add routes for category, course details page [27.06.2025]

--- FrontEnd/src/routes/mainRoutes.jsx
+++ FrontEnd/src/routes/mainRoutes.jsx
@@ -2,15 +2,21 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import HomePage from '../components/HomePage/HomePage';
-import FaqsPage from '../pages/MainPage/FaqsPage';
-import ProfileRoutes from './profileRoutes';
+import HomePage from "../components/HomePage/HomePage";
+import FaqsPage from "../pages/MainPage/FaqsPage";
+import ProfileRoutes from "./profileRoutes";
+import CategoryPage from "../components/Categories/CategoryPage";
+import SingleCourse from "../components/CourseDetails/SingleCourse";
 
 const mainRoutesContent = (
   <>
     <Route index element={<HomePage />} />
     <Route path="faqs" element={<FaqsPage />} />
     <Route path="profile/*" element={<ProfileRoutes />} />
+    <Route path="category" element={<CategoryPage />} />
+    <Route path="coursedetail" element={<SingleCourse />} />
+    {/*Course Detail sau khi thêm data sẽ đổi lại thành course/:id */}
+    {/* Thêm các route chính khác ở đây */}
     {/* <Route path="courses" element={<CoursesPage />} /> */}
     {/* Thêm các route chính khác ở đây */}
   </>
