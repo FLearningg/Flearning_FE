a63d225f9c7242410e5dfba52ffb8974b4b30f86
Trung <trungnnde180575@fpt.edu.vn>
2025-07-04 13:45:23 +0700

    Update authService.js

--- FrontEnd/src/services/authService.js
+++ FrontEnd/src/services/authService.js
@@ -4,8 +4,7 @@ import { logout } from '../store/authSlice';
 
 // Tạo một instance của axios với cấu hình chung
 const apiClient = axios.create({
-    // baseURL: 'https://flearning-api-a5h6hbcphdcbhndv.southeastasia-01.azurewebsites.net/api', // URL gốc của API backend
-    baseURL: 'http://localhost:5000/api', // URL gốc của API backend trong môi trường phát triển
+    baseURL: 'https://flearning-api-a5h6hbcphdcbhndv.southeastasia-01.azurewebsites.net/api', // URL gốc của API backend
     withCredentials: true, // Cho phép trình duyệt tự động gửi cookie
 });
 
