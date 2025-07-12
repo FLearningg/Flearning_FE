8b838d529c44599d652122a8c134823c1eb647ef
Nekomimi <kthkun@gmail.com>
2025-07-12 23:20:47 +0700

    [Dat][UI] Add Forgot Password & Dashboard Stats UI #12.07.2025 (#81)

--- FrontEnd/src/routes/authRoutes.jsx
+++ FrontEnd/src/routes/authRoutes.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import LoginPage from '../pages/AuthPage/LoginPage';
-import SignUpPage from '../pages/AuthPage/SignUpPage';
-import VerifyEmailPage from '../pages/AuthPage/VerifyEmailPage';
-import RegistrationSuccessPage from '../pages/AuthPage/RegistrationSuccessPage';
+import LoginPage from "../pages/AuthPage/LoginPage";
+import SignUpPage from "../pages/AuthPage/SignUpPage";
+import VerifyEmailPage from "../pages/AuthPage/VerifyEmailPage";
+import RegistrationSuccessPage from "../pages/AuthPage/RegistrationSuccessPage";
+import ForgotPasswordPage from "../pages/AuthPage/ForgotPasswordPage";
+import ResetPasswordPage from "../pages/AuthPage/ResetPasswordPage";
 
-
-const authRoutesContent = ( // Đây là JSX, không phải là một component
-  <>
-    <Route path="login" element={<LoginPage />} />
-    <Route path="signup" element={<SignUpPage />} />
-    <Route path="/registration-success" element={<RegistrationSuccessPage />} />
-    <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
-  </>
-);
+const authRoutesContent = // Đây là JSX, không phải là một component
+  (
+    <>
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignUpPage />} />
+      <Route
+        path="/registration-success"
+        element={<RegistrationSuccessPage />}
+      />
+      <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+    </>
+  );
 
 export default authRoutesContent; // Export trực tiếp JSX fragment
