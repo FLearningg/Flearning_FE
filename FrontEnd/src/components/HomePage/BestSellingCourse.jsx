0f3a4efbb16a60a2dc5c24cda6f38721d57657f8
Nguyễn Ngọc Trung <trungnnde180575@fpt.edu.vn>
2025-07-13 19:54:30 +0700

    fix giá chưa làm tròn (#82)

--- FrontEnd/src/components/HomePage/BestSellingCourse.jsx
+++ FrontEnd/src/components/HomePage/BestSellingCourse.jsx
@@ -40,7 +40,10 @@ function BestSellingCourse() {
       cardProps: {
         image: course.thumbnail,
         category: course?.categoryIds?.[0]?.name || "Uncategorized",
-        price: `${course.price}$`,
+        // price: `${course.price}$`,
+        price: Number.isInteger(finalPrice)
+          ? `${finalPrice}$`
+          : `${parseFloat(finalPrice.toFixed(2))}$`,
         title: course.title,
         rating: course?.rating || 0, // If there is a rating field, take it, otherwise 0
         students: course.studentsEnrolled?.length || 0,
@@ -56,8 +59,8 @@ function BestSellingCourse() {
         students: course.studentsEnrolled?.length || 0,
         level: course.level,
         duration: course.duration,
-        price: `${finalPrice}$`,
-        oldPrice: course.discountId ? `${course.price}$` : "",
+        price: `${finalPrice.toFixed(2)}$`,
+        oldPrice: course.discountId ? `${course.price.toFixed(2)}$` : "",
         discount: discountText,
         learnList: course.detail?.willLearn || [],
       },
